123bed160eefd0cd82c4b0fb68f794c54910f982
Jonas Flodstrøm <jonflo14@imkatta.no>
2025-03-05 19:43:17 +0100

    Added functioning swipe function (Only UkeMeny)
    installed react-swipeable and created so you are able to swipe to switch day, as well as change day on calendarpopup. "This has no animation for now".

--- app/ukemeny/page.tsx
+++ app/ukemeny/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import { useSwipeable } from "react-swipeable";
 import Header from "@/components/templates/Header";
 import DagensMeny from "@/components/templates/Dagens/DagensMeny";
 import DagensContent from "@/components/templates/Dagens/DagensContent";
@@ -12,8 +13,26 @@ export default function UkeMeny() {
   const [selectedDay, setSelectedDay] = useState(getToday());
   const selectedMeal = menuData.find((item) => item.day === selectedDay);
 
+  const handleSwipe = (direction: string) => {
+    const currentIndex = menuData.findIndex((item) => item.day === selectedDay);
+    let newIndex = currentIndex;
+
+    if (direction === "LEFT") {
+      newIndex = (currentIndex + 1) % menuData.length;
+    } else if (direction === "RIGHT") {
+      newIndex = (currentIndex - 1 + menuData.length) % menuData.length;
+    }
+
+    setSelectedDay(menuData[newIndex].day);
+  };
+
+  const swipeHandlers = useSwipeable({
+    onSwipedLeft: () => handleSwipe("LEFT"),
+    onSwipedRight: () => handleSwipe("RIGHT"),
+  });
+
   return (
-    <>
+    <div {...swipeHandlers} style={{ touchAction: "pan-y" }}>
       <Header selectedDay={selectedDay} setSelectedDay={setSelectedDay} />
       {selectedMeal && (
         <>
@@ -29,6 +48,6 @@ export default function UkeMeny() {
         </>
       )}
       <NavBar />
-    </>
+    </div>
   );
 }
