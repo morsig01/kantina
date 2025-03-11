1f2b3f368ee596fc59009d971bf81a0474eb6681
Jonas Flodstrøm <jonflo14@imkatta.no>
2025-03-11 09:16:26 +0100

    Implemented varer (Sanity)

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,7 +1,8 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import { meals } from './meal.schema'
-import { weekMeals } from './week-meals.schema'
+import { type SchemaTypeDefinition } from "sanity";
+import { meals } from "./meal.schema";
+import { weekMeals } from "./week-meals.schema";
+import { vare } from "./varer.schema";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [meals, weekMeals],
-}
+  types: [meals, weekMeals, vare],
+};
